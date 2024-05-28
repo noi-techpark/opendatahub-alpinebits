cd544525c7b842dc520d00f402a79efa2d074d0d
gappc <gapp.christian@gmail.com>
2024-05-28 22:27:43 +0200

    Move SPDX header to top of files

--- alpinebits-validation/impl/src/test/java/it/bz/opendatahub/alpinebits/validation/schema/v_2022_10/inventory/fromxml/XmlDataProvider.java
+++ alpinebits-validation/impl/src/test/java/it/bz/opendatahub/alpinebits/validation/schema/v_2022_10/inventory/fromxml/XmlDataProvider.java
@@ -1,12 +1,12 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: MPL-2.0
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
-// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
-//
-// SPDX-License-Identifier: MPL-2.0
 
 package it.bz.opendatahub.alpinebits.validation.schema.v_2022_10.inventory.fromxml;
 
